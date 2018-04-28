3390bbc02fd685f1cab14ba896acdcb7a89b8951
cgspine <cgspine@gmail.com>
2018-04-28 11:07:33 +0800

    fix the problem of launching double activity

--- qmuidemo/src/main/java/com/qmuiteam/qmuidemo/activity/LauncherActivity.java
+++ qmuidemo/src/main/java/com/qmuiteam/qmuidemo/activity/LauncherActivity.java
@@ -16,6 +16,10 @@ public class LauncherActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
+            finish();
+            return;
+        }
         Intent intent = new Intent(this, QDMainActivity.class);
         startActivity(intent);
         finish();
