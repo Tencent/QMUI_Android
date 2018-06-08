0fa27ca27fb9b0e45988cba2587656e180d3c4bc
cgspine <cgspine@gmail.com>
2018-06-08 19:54:05 +0800

    add method generateHorIconText

--- qmui/src/main/java/com/qmuiteam/qmui/util/QMUISpanHelper.java
+++ qmui/src/main/java/com/qmuiteam/qmui/util/QMUISpanHelper.java
@@ -24,36 +24,39 @@ public class QMUISpanHelper {
      * @return 返回带有 icon 的文字
      */
     public static CharSequence generateSideIconText(boolean left, int iconPadding, CharSequence text, Drawable icon) {
-        if (icon == null) {
+        return generateHorIconText(text, left ? iconPadding : 0, left ? icon : null, left ? 0 : iconPadding, left ? null : icon);
+    }
+
+    public static CharSequence generateHorIconText(CharSequence text, int leftPadding, Drawable iconLeft, int rightPadding, Drawable iconRight) {
+        if (iconLeft == null && iconRight == null) {
             return text;
         }
-
-        icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
         String iconTag = "[icon]";
         SpannableStringBuilder builder = new SpannableStringBuilder();
         int start, end;
-        if (left) {
+        if (iconLeft != null) {
+            iconLeft.setBounds(0, 0, iconLeft.getIntrinsicWidth(), iconLeft.getIntrinsicHeight());
             start = 0;
             builder.append(iconTag);
             end = builder.length();
-            builder.append(text);
-        } else {
-            builder.append(text);
+
+            QMUIMarginImageSpan imageSpan = new QMUIMarginImageSpan(iconLeft, QMUIAlignMiddleImageSpan.ALIGN_MIDDLE, 0, leftPadding);
+            imageSpan.setAvoidSuperChangeFontMetrics(true);
+            builder.setSpan(imageSpan, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        }
+
+        builder.append(text);
+        if (iconRight != null) {
+            iconRight.setBounds(0, 0, iconRight.getIntrinsicWidth(), iconRight.getIntrinsicHeight());
             start = builder.length();
             builder.append(iconTag);
             end = builder.length();
+
+            QMUIMarginImageSpan imageSpan = new QMUIMarginImageSpan(iconRight, QMUIAlignMiddleImageSpan.ALIGN_MIDDLE, rightPadding, 0);
+            imageSpan.setAvoidSuperChangeFontMetrics(true);
+            builder.setSpan(imageSpan, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
         }
 
-        QMUIMarginImageSpan imageSpan;
-
-        if (left) {
-            imageSpan = new QMUIMarginImageSpan(icon, QMUIAlignMiddleImageSpan.ALIGN_MIDDLE, 0, iconPadding);
-        } else {
-            imageSpan = new QMUIMarginImageSpan(icon, QMUIAlignMiddleImageSpan.ALIGN_MIDDLE, iconPadding, 0);
-        }
-
-        builder.setSpan(imageSpan, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        imageSpan.setAvoidSuperChangeFontMetrics(true);
         return builder;
     }
 }
