3c844d51fe255c10ed521c08d26e5ecc9ccc653a
cgspine <cgspine@gmail.com>
2019-04-28 21:21:29 +0800

    move QMUIContinuousScrollLayout to components.

--- qmuidemo/src/main/java/com/qmuiteam/qmuidemo/fragment/lab/QDContinuousNestedScrollFragment.java
+++ qmuidemo/src/main/java/com/qmuiteam/qmuidemo/fragment/lab/QDContinuousNestedScrollFragment.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.qmuiteam.qmui.nestedScroll.QMUIContinuousNestedScrollLayout;
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
 import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView;
 import com.qmuiteam.qmuidemo.R;
@@ -32,7 +33,9 @@ import androidx.annotation.Nullable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-@Widget(group = Group.Lab, name = "continuous nested scroll", iconRes = R.mipmap.icon_grid_in_progress)
+@Widget(group = Group.Lab,
+        widgetClass = QMUIContinuousNestedScrollLayout.class,
+        iconRes = R.mipmap.icon_grid_in_progress)
 public class QDContinuousNestedScrollFragment extends BaseFragment {
 
     @BindView(R.id.topbar)
