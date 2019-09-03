39f2f198388d224527184bf609fc3c9bd444581a
cgspine <cgspine@gmail.com>
2019-09-03 19:38:25 +0800

    change super class of QMUIAlphaTextView

--- qmui/src/main/java/com/qmuiteam/qmui/alpha/QMUIAlphaTextView.java
+++ qmui/src/main/java/com/qmuiteam/qmui/alpha/QMUIAlphaTextView.java
@@ -17,13 +17,14 @@
 package com.qmuiteam.qmui.alpha;
 
 import android.content.Context;
-import androidx.appcompat.widget.AppCompatTextView;
 import android.util.AttributeSet;
+
+import com.qmuiteam.qmui.widget.textview.QMUISpanTouchFixTextView;
 
 /**
  * 在 pressed 和 disabled 时改变 View 的透明度
  */
-public class QMUIAlphaTextView extends AppCompatTextView implements QMUIAlphaViewInf {
+public class QMUIAlphaTextView extends QMUISpanTouchFixTextView implements QMUIAlphaViewInf {
 
     private QMUIAlphaViewHelper mAlphaViewHelper;
 
@@ -47,8 +48,8 @@ public class QMUIAlphaTextView extends AppCompatTextView implements QMUIAlphaVie
     }
 
     @Override
-    public void setPressed(boolean pressed) {
-        super.setPressed(pressed);
+    protected void onSetPressed(boolean pressed) {
+        super.onSetPressed(pressed);
         getAlphaViewHelper().onPressedChanged(this, pressed);
     }
 
