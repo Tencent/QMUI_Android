3a9d9371b1a8a99a489802cfd5ac6555defa5b49
cgspine <cgspine@gmail.com>
2020-01-05 16:24:14 +0800

    Open api for sub class.

--- qmui/src/main/java/com/qmuiteam/qmui/widget/webview/QMUIBridgeWebViewClient.java
+++ qmui/src/main/java/com/qmuiteam/qmui/widget/webview/QMUIBridgeWebViewClient.java
@@ -1,5 +1,6 @@
 package com.qmuiteam.qmui.widget.webview;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.webkit.WebResourceRequest;
@@ -28,16 +29,20 @@ public class QMUIBridgeWebViewClient extends QMUIWebViewClient {
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    public final boolean shouldOverrideUrlLoading(WebView view, String url) {
         if (url.startsWith(QMUI_BRIDGE_HAS_MESSAGE)) {
             mWebViewBridgeHandler.fetchAndMessageFromJs();
             return true;
         }
+        return onShouldOverrideUrlLoading(view, url);
+    }
+
+    protected boolean onShouldOverrideUrlLoading(WebView view, String url){
         return super.shouldOverrideUrlLoading(view, url);
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+    public final boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             String url = request.getUrl().toString();
             if (url.startsWith(QMUI_BRIDGE_HAS_MESSAGE)) {
@@ -46,6 +51,11 @@ public class QMUIBridgeWebViewClient extends QMUIWebViewClient {
             }
         }
 
+        return onShouldOverrideUrlLoading(view, request);
+    }
+
+    @TargetApi(24)
+    protected boolean onShouldOverrideUrlLoading(WebView view, WebResourceRequest request){
         return super.shouldOverrideUrlLoading(view, request);
     }
 
