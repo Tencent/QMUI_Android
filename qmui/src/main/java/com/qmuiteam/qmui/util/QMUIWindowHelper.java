83b3bb0f2cf175007dd49d7b11c5052ff2ac13b1
cgspine <cgspine@gmail.com>
2020-02-23 21:21:50 +0800

    bugfix: Android 10+ can not reflect View.mAttachInfo anymore.

--- qmui/src/main/java/com/qmuiteam/qmui/util/QMUIWindowHelper.java
+++ qmui/src/main/java/com/qmuiteam/qmui/util/QMUIWindowHelper.java
@@ -19,12 +19,14 @@ package com.qmuiteam.qmui.util;
 import android.graphics.Rect;
 import android.os.Build;
 import android.view.View;
+import android.view.ViewParent;
 import android.view.WindowManager;
 
 import com.qmuiteam.qmui.BuildConfig;
 
 import java.lang.reflect.Field;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 /**
@@ -54,13 +56,13 @@ public class QMUIWindowHelper {
 
     @Nullable
     @SuppressWarnings({"JavaReflectionMemberAccess"})
-    public static Rect unSafeGetWindowVisibleInsets(View view) {
+    public static Rect unSafeGetWindowVisibleInsets(@NonNull View view) {
+        Object attachInfo = getAttachInfoFromView(view);
+        if(attachInfo == null){
+            return null;
+        }
         try {
-            // Android P forbid the reflection for @hide filed,
             // fortunately now it is in light greylist, just be warned.
-            Field field = View.class.getDeclaredField("mAttachInfo");
-            field.setAccessible(true);
-            Object attachInfo = field.get(view);
             Field visibleInsetsField = attachInfo.getClass().getDeclaredField("mVisibleInsets");
             visibleInsetsField.setAccessible(true);
             Object visibleInsets = visibleInsetsField.get(attachInfo);
@@ -77,13 +79,13 @@ public class QMUIWindowHelper {
 
     @Nullable
     @SuppressWarnings({"JavaReflectionMemberAccess"})
-    public static Rect unSafeGetContentInsets(View view) {
+    public static Rect unSafeGetContentInsets(@NonNull View view) {
+        Object attachInfo = getAttachInfoFromView(view);
+        if(attachInfo == null){
+            return null;
+        }
         try {
-            // Android P forbid the reflection for @hide filed,
             // fortunately now it is in light greylist, just be warned.
-            Field field = View.class.getDeclaredField("mAttachInfo");
-            field.setAccessible(true);
-            Object attachInfo = field.get(view);
             Field visibleInsetsField = attachInfo.getClass().getDeclaredField("mContentInsets");
             visibleInsetsField.setAccessible(true);
             Object visibleInsets = visibleInsetsField.get(attachInfo);
@@ -96,5 +98,41 @@ public class QMUIWindowHelper {
             }
         }
         return null;
+    }
+
+    public static Object getAttachInfoFromView(@NonNull View view){
+        Object attachInfo = null;
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+            // Android 10+ can not reflect the View.mAttachInfo
+            // fortunately now it is in light greylist in ViewRootImpl
+            View rootView = view.getRootView();
+            if(rootView != null){
+                ViewParent vp = rootView.getParent();
+                if(vp != null){
+                    try {
+                        Field field = vp.getClass().getDeclaredField("mAttachInfo");
+                        field.setAccessible(true);
+                        attachInfo = field.get(vp);
+                    } catch (Throwable e) {
+                        if (BuildConfig.DEBUG) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }else{
+            try {
+                // Android P forbid the reflection for @hide filed,
+                // fortunately now it is in light greylist, just be warned.
+                Field field = View.class.getDeclaredField("mAttachInfo");
+                field.setAccessible(true);
+                attachInfo = field.get(view);
+            } catch (Throwable e) {
+                if (BuildConfig.DEBUG) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return attachInfo;
     }
 }
