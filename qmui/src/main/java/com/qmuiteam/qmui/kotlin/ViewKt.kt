6bcaf14f281ed7bf2841e463e11837567f673c02
libreui <libre@aliyun.com>
2020-03-12 16:20:13 +0800

    Update ViewKt.kt
    1.System.currentTimeMillis返回Long，写入Tag后将无法专为Int，导致if块持续进入，仿连触失效
    2.防止连续点击，如果block执行时间过长，setTag将无法执行

--- qmui/src/main/java/com/qmuiteam/qmui/kotlin/ViewKt.kt
+++ qmui/src/main/java/com/qmuiteam/qmui/kotlin/ViewKt.kt
@@ -9,10 +9,10 @@ fun throttleClick(wait: Long = 200, block: ((View) -> Unit)): View.OnClickListen
 
     return View.OnClickListener { v ->
         val current = System.currentTimeMillis()
-        val lastClickTime = (v.getTag(R.id.qmui_click_timestamp) as? Int) ?: 0
+        val lastClickTime = (v.getTag(R.id.qmui_click_timestamp) as? Long) ?: 0
         if (current - lastClickTime > wait) {
-            block(v)
             v.setTag(R.id.qmui_click_timestamp, current)
+            block(v)
         }
     }
 }
