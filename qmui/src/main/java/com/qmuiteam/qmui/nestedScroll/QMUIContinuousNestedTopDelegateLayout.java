3aa4240ca8e90318c1b6b650fb3d89a042e34e71
cgspine <cgspine@gmail.com>
2020-05-19 23:37:20 +0800

    bugfix: protection

--- qmui/src/main/java/com/qmuiteam/qmui/nestedScroll/QMUIContinuousNestedTopDelegateLayout.java
+++ qmui/src/main/java/com/qmuiteam/qmui/nestedScroll/QMUIContinuousNestedTopDelegateLayout.java
@@ -550,13 +550,15 @@ public class QMUIContinuousNestedTopDelegateLayout extends FrameLayout implement
             }
         } else if (unconsumed < 0) {
             int bottomMargin = getPaddingBottom() + (mFooterView != null ? mFooterView.getHeight() : 0);
-            int b = mOffsetRange - bottomMargin;
-            if (mOffsetCurrent + unconsumed >= b) {
-                offsetTo(mOffsetCurrent + unconsumed);
-                consumed[1] += unconsumed;
-            } else if (mOffsetCurrent > b) {
-                consumed[1] += b - mOffsetCurrent;
-                offsetTo(b);
+            if(mOffsetRange > bottomMargin){
+                int b = mOffsetRange - bottomMargin;
+                if (mOffsetCurrent + unconsumed >= b) {
+                    offsetTo(mOffsetCurrent + unconsumed);
+                    consumed[1] += unconsumed;
+                } else if (mOffsetCurrent > b) {
+                    consumed[1] += b - mOffsetCurrent;
+                    offsetTo(b);
+                }
             }
         }
     }
