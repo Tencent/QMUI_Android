81cc6773e46155a002fa7d52921b7c09e154ba90
cgspine <cgspine@gmail.com>
2022-04-11 16:38:27 +0800

    feat: update agp to 7.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 
